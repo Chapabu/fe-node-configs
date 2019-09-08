322d098d3badcbdcfd2951604107b457977c009b
Matt Chapman <chapabu101@gmail.com>
2019-09-08 02:07:31 +0100

    Add basic Stylelint config.

---
+++ packages/stylelint/index.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-rational-order',
+    'stylelint-a11y/recommended'
+  ],
+  rules: {
+    'selector-max-id': 0
+  }
+};
