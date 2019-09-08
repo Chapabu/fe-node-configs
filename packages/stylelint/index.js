97b8f27f7d91e05c2078ba8f72aff3f4981cc26e
Matt Chapman <chapabu101@gmail.com>
2019-09-08 02:08:35 +0100

    Add Prettier support.

--- packages/stylelint/index.js
+++ packages/stylelint/index.js
@@ -2,7 +2,8 @@ module.exports = {
   extends: [
     'stylelint-config-standard',
     'stylelint-config-rational-order',
-    'stylelint-a11y/recommended'
+    'stylelint-a11y/recommended',
+    'stylelint-prettier/recommended'
   ],
   rules: {
     'selector-max-id': 0
